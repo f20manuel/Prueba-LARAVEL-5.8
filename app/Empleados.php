b0750a78c96619b8d74d0aa370600016b1943a35
Manuel Fernandez <f20manuel@gmail.com>
2019-09-30 12:06:19 -0500

    Tablas empresas, empleados creadas con foreign

---
+++ app/Empleados.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Empleados extends Model
+{
+    protected $fillable = [
+        'first_name', 'last_name', 'company_id', 'email', 'phone'
+    ]
+}
---
+++ database/migrations/2019_09_30_174743_create_empleados_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->unsignedBigInteger('company_id');
+            $table->string('email')->unique();
+            $table->integer('phone')->nullablle()->unique();
+            $table->timestamps();
+
+            $table->foreign('company_id')->references('id')->on('empresas')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('empleados', function (Blueprint $table) {
+            $table->dropForeign(['company_id']);
+            $table->dropColumn('company_id');
+        });
+        Schema::dropIfExists('empleados');
+    }
+}
