71017f932bac1bc66911515e5ac9ca71a84a363c
Manuel Fernandez <f20manuel@gmail.com>
2019-09-30 12:31:49 -0500

    Rol de administrador asignado

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = 'Admin';
+        $user->email = 'admin@admin.com';
+        $user->password = Hash::make('password');
+        $user->save();
+
+        $user->assignRole('Admin');
+    }
+}
